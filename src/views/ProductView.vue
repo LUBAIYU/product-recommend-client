d92edca03510c6edd40e40e751b1b74dc9e30ca7
user <1296800094@qq.com>
2024-05-01 14:07:15 +0800

    开发商品推荐功能

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,40 +1,30 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import ProductList from '@/components/ProductList.vue'
 import { recommendProductsAPI } from '@/apis/product'
 import { showFailToast } from 'vant'
 
 //商品列表
 const productList = ref<API.ProductInfo[]>([])
-//当前页码
-const current = ref<number>(1)
-//每页记录数
-const pageSize = ref<number>(5)
-//总记录数
-const total = ref<number>(0)
 
 //商品推荐
 const recommendProducts = async () => {
   const res = await recommendProductsAPI({
-    current: current.value,
-    pageSize: pageSize.value
+    count: 5
   })
   if (res.code === 200) {
-    productList.value = res.data.records
+    productList.value = res.data
   } else {
     showFailToast(res.message)
   }
 }
 
-//onMounted(() => recommendProducts())
+onMounted(() => recommendProducts())
 </script>
 
 <template>
   <van-cell title="猜您喜欢" v-if="productList&&productList.length>0" />
   <ProductList :product-list="productList" />
-  <van-pagination style="margin-top: 10px" v-if="productList&&productList.length>0" v-model="current"
-                  :total-items="total"
-                  :items-per-page="pageSize"></van-pagination>
   <van-empty description="暂无商品" v-if="!productList||productList.length<1"></van-empty>
 </template>
 
